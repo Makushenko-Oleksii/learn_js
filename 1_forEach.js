b6923696b8a483f5dddb2f7f07b7ed235de98549
Makushenko Oleksii <makushenkooleksii@gmail.com>
2025-04-15 14:18:15 +0300

    start middle

--- 1_forEach.js
+++ 1_forEach.js
@@ -1,3 +1,4 @@
+//Junior
 // 1.Дан массив чисел. Используя forEach, выведите каждый элемент массива в консоль.
 
 let arr = [2, 3, 5, 6, 8, 9];
@@ -28,3 +29,32 @@ arr4.forEach(item => console.log(item.name));
 let arr5 = [2, 3, 5, 6, 8, 9];
 let res = 0;
 arr5.forEach(item => console.log(res += item));
+
+//Middle
+// 1. Дан массив чисел. Используя forEach, создайте новый массив, содержащий только четные числа.
+let arr6 = [2, 3, 5, 6, 8, 9];
+let evenArr = [];
+arr6.forEach(item => {
+    if (item % 2 === 0) {
+        evenArr.push(item);
+    }
+});
+console.log(evenArr);
+
+// 2. Дан массив объектов {id: number, value: number}. Измените значение value каждого объекта, прибавив к нему его id.
+
+arr7 = [
+    {id: 1, value: 7},
+    {id: 2, value: 8},
+    {id: 3, value: 9},
+    {id: 4, value: 6},
+    {id: 5, value: 5},
+    {id: 6, value: 4},
+]
+
+arr7.forEach(item => {
+   item.value= item.value + item.id
+});
+
+console.log(arr7);
+
